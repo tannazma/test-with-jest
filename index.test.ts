efd4de7cea41dbf44aaf79569f1d4979e137f6b7
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-07 12:15:30 +0100

    created multiply, divide and prime function + tests

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,23 @@
-import { sum, subtract } from "./index";
- 
+import { sum, subtract, multiply, divide, primeNumbers } from "./index";
+
 test("two plus two is four", () => {
-    // const summedNumbers = 2 + 2;
-    expect(sum(2,2)).toBe(4);
-  });
- 
+  // const summedNumbers = 2 + 2;
+  expect(sum(2, 2)).toBe(4);
+});
+
 test("subtract function", () => {
   expect(subtract(10, 7)).toBe(3);
+});
+
+test("multiply function", () => {
+  expect(multiply(10, 2)).toBe(20);
+});
+
+test("divide function", () => {
+  expect(divide(10, 2)).not.toBe(6);
+});
+
+test("prime numbers function", () => {
+  expect(primeNumbers).toContain(7);
+  expect(primeNumbers).not.toContain(8);
 });
--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 export const sum = (a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
+
+export const multiply = (a: number, b: number) => a * b;
+
+export const divide = (a: number, b: number) => a / b;
+
+export const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
